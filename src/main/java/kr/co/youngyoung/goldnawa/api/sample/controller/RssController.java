0102dd9647f9c7931004955d474cb360c1716cb9
devrartes <devrartes@gmail.com>
2021-04-09 17:22:27 +0900

    # rss

--- src/main/java/kr/co/youngyoung/goldnawa/api/sample/controller/RssController.java
+++ src/main/java/kr/co/youngyoung/goldnawa/api/sample/controller/RssController.java
@@ -25,10 +25,8 @@ public class RssController extends BaseController {
     @Value("${spring.profiles}")
     String profile;
 
-    @GetMapping(path = "/rss")
-    @ApiOperation(value = "샘플 매핑",
-            notes = "이것은 샘플 매핑입니다")
-    public void sample(HttpServletRequest request, HttpServletResponse response) throws IOException, JDOMException {
+    @GetMapping(path = "/rss/pressRelease")
+    public void pressRelease(HttpServletRequest request, HttpServletResponse response) throws IOException, JDOMException {
         String path = "";
         if( "local".equals(profile) ){
             path = "C:\\workspace\\newsroom-front\\web\\WebContent\\UPLOAD\\RSS\\RssPressRelease.xml";
@@ -36,63 +34,39 @@ public class RssController extends BaseController {
             path = "/home/devrartes/RssPressRelease.xml";
         }
 
+        SAXBuilder builder = new SAXBuilder();
+        Document doc = builder.build(path);
+
+        RssWriter.responseStreamWriter(doc, response);
+    }
+
+    @GetMapping(path = "/rss/rssOurStory")
+    public void rssOurStory(HttpServletRequest request, HttpServletResponse response) throws IOException, JDOMException {
+        String path = "";
+        if( "local".equals(profile) ){
+            path = "C:\\workspace\\newsroom-front\\web\\WebContent\\UPLOAD\\RSS\\RssOurStory.xml";
+        }else{
+            path = "/home/devrartes/RssPressRelease.xml";
+        }
 
         SAXBuilder builder = new SAXBuilder();
         Document doc = builder.build(path);
 
         RssWriter.responseStreamWriter(doc, response);
-/*// 1. Document 생성
-        Document doc = new Document();
+    }
 
-        Namespace namespace = Namespace.getNamespace("content", "http://purl.org/rss/1.0/modules/content/");
-// 2. Root Element 생성
-        Element root = new Element("rss");
-        root.addNamespaceDeclaration(namespace);
-        root.setAttribute("version", "2.0");
-        doc.setContent(root);
-
-// 3. Child Element 생성
-        Element records = new Element("records");
-        records.setNamespace(namespace);
-        root.addContent(records);
-
-        for( int i = 1; i <= 5; i++ ) {
-            Element record = new Element("record");
-            record.addContent(new CDATA("<그대로 출력되는 문자열>"));
-            records.addContent(record);
+    @GetMapping(path = "/rss/trendInsight")
+    public void trendInsight(HttpServletRequest request, HttpServletResponse response) throws IOException, JDOMException {
+        String path = "";
+        if( "local".equals(profile) ){
+            path = "C:\\workspace\\newsroom-front\\web\\WebContent\\UPLOAD\\RSS\\RssTrendInsight.xml";
+        }else{
+            path = "/home/devrartes/RssPressRelease.xml";
         }
 
-// 4. 파일에 출력
-        OutputStreamWriter writer = null;
-        try {
-            writer = new OutputStreamWriter(response.getOutputStream());
-            new XMLOutputter().output(doc, writer);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            writer.close();
-        }
+        SAXBuilder builder = new SAXBuilder();
+        Document doc = builder.build(path);
 
-// 5. 문자열 출력 (Default Format : Format.getCompactFormat())
-        String xml = new XMLOutputter().outputString(doc);
-        System.out.println(xml);
-
-// 6. 출력 포맷결정
-// 6-1. XML형식 이쁜 포맷
-        xml = new XMLOutputter(Format.getPrettyFormat()).outputString(doc);
-        System.out.println(xml);
-
-// 6-2. 단순한 문자열 포맷
-        xml = new XMLOutputter(Format.getCompactFormat()) {
-            @Override
-            public void printDeclaration(Writer out, Document doc, String encoding) throws IOException {
-                out.write("<!--?xml version=\"1.0\" encoding=\"" + encoding + "\"?-->");
-            }
-        }.outputString(doc);
-        System.out.println(xml);
-
-// 7. XML 인코딩을 변경하고 싶으면
-        Format.getCompactFormat().setEncoding("UTF-8");*/
+        RssWriter.responseStreamWriter(doc, response);
     }
 }
--- src/main/java/kr/co/youngyoung/goldnawa/common/rss/util/RssWriter.java
+++ src/main/java/kr/co/youngyoung/goldnawa/common/rss/util/RssWriter.java
@@ -1,24 +1,34 @@
 package kr.co.youngyoung.goldnawa.common.rss.util;
 
 import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class RssWriter {
+
     /**
-     * FilePath에 해당하는 XML 파일 생성
-     * 
+     * 해당 파일 패스 경로에 XML 파일 생성
+     *
      * @param doc
      * @param filePath
+     * @param fileName
      */
-    static public void fileWriter(Document doc, String filePath) {
-        try(FileOutputStream writer = new FileOutputStream(filePath)) {
+    static public void fileWriter(Document doc, String filePath, String fileName) {
+        //폴더 생성
+        File targetFile = new File(filePath);
+        if (!targetFile.exists()) {
+            targetFile.mkdirs();
+        }
+        //파일 생성
+        try(FileOutputStream writer = new FileOutputStream(filePath+"/"+fileName)) {
             new XMLOutputter().output(doc, writer);
         } catch (FileNotFoundException fnfe) {
             throw new RuntimeException(fnfe.getMessage() + "\n" + "File Not Found.");
@@ -37,6 +47,23 @@ public class RssWriter {
         try(OutputStreamWriter writer = new OutputStreamWriter(response.getOutputStream())) {
             new XMLOutputter().output(doc, writer);
         } catch (IOException e) {
+            throw new RuntimeException(e.getMessage() + "\n" + "XML generation failed.");
+        }
+    }
+
+    /**
+     * HttpServletResponse의 OutputStream을 받아 reponse에 담음
+     *
+     * @param path
+     * @param response
+     */
+    static public void responseStreamWriter(String path, HttpServletResponse response) {
+        try(OutputStreamWriter writer = new OutputStreamWriter(response.getOutputStream())) {
+            SAXBuilder builder = new SAXBuilder();
+            Document doc = builder.build(path);
+
+            new XMLOutputter().output(doc, writer);
+        } catch (IOException | JDOMException e) {
             throw new RuntimeException(e.getMessage() + "\n" + "XML generation failed.");
         }
     }
